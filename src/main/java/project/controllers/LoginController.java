1db94d5ac3db5c1b424f759d2fee369a46da4171
Yaroslav <yaroslav8oroboro@gmail.com>
2021-01-24 22:29:33 +0200

    added constant 'driver_id' in the LoginController class

--- src/main/java/project/controllers/LoginController.java
+++ src/main/java/project/controllers/LoginController.java
@@ -12,6 +12,7 @@ import project.models.Driver;
 import project.security.AuthenticationService;
 
 public class LoginController extends HttpServlet {
+    private static final String DRIVER_ID = "driver_id";
     public static final Injector injector = Injector.getInstance("project");
     private final AuthenticationService authenticationService = (AuthenticationService)
             injector.getInstance(AuthenticationService.class);
@@ -30,7 +31,7 @@ public class LoginController extends HttpServlet {
         try {
             Driver driver = authenticationService.login(login, password);
             HttpSession session = req.getSession();
-            session.setAttribute("driver_id", driver.getId());
+            session.setAttribute(DRIVER_ID, driver.getId());
         } catch (AuthenticationException ex) {
             req.setAttribute("error", ex.getMessage());
             req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
